7b9dcb0a9f1ed1ba24293c704c2ffa2f31795a6c
Sumit Yadav <ysumit99@gmail.com>
2017-06-29 12:36:53 +0530

    Basic layout of the page done!

--- weather_predict.php
+++ weather_predict.php
@@ -1,17 +1,75 @@
 <!doctype html>
 	<html>
 		<head>
-			<title>My First Webpage</title>	 
+			<title>Weather Predictor</title>	 
 			<meta charset="utf-8" />	
 			<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
 			<meta name="viewport" content="width=device-width, initial-scale=1" />
 				 <!-- Latest compiled and minified CSS -->	
 				<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />	
 				 <!-- Optional theme -->	
+
+		<style>
+
+			html,body {
+				height:100%;
+			}
+
+			.container{
+				background-image: url("background5.jpg");
+				width: 100%;
+				height: 100%;
+				background-size: cover;
+				background-position: center;
+				padding-top: 150px;
+			}
+
+			.center {
+				text-align: center;
+			}
+			.white{
+				color: white;
+			}
+			p{
+				padding-top: 15px;
+				padding-bottom: 15px;
+			}
+
+			button{
+				margin-top: 20px;
+			}
+			
+		</style>
 				 
 				 
  		</head>	
   		<body>	 
+
+  			<div class="container">
+  				<div class="row">
+  					<div class="col-md-6 col-md-offset-3 center">
+  						
+  						<h1 class="center white">Weather Predictor</h1>
+
+  						<p class="lead center white">Enter your city below to get a forcast of weather.</p>
+
+  						<form>
+  							<div class="form-group">
+  								<input type="text" class="form-control" name="city" placeholder="Eg. Mumbai, Delhi, Bangalore..." id="city">
+
+  							</div>
+  								
+  								<button class=" btn btn-success btn-lg ">Find My Weather
+  								</button>
+  								
+  								
+  							
+  						</form>
+  						
+  					</div>
+  					
+  				</div>
+  			</div>
   		
   				
 					<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
